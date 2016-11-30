23cb837bc98d6f21021df57d96167b664bb7ae7e
agc123 <agc123@yeah.net>
2016-11-30 00:20:35 -0500

    full scrren

--- public/room.js
+++ public/room.js
@@ -2,13 +2,18 @@ var gl;
 
 function initGL(canvas) {
     try {
-	gl = canvas.getContext("experimental-webgl", {alpha:false});
-	gl.viewportWidth = canvas.width;
-	gl.viewportHeight = canvas.height;
+        gl = canvas.getContext("experimental-webgl", {alpha:false});
+        gl.viewportWidth = canvas.width;
+        gl.viewportHeight = canvas.height;
+    	
+
+        
+
     } catch (e) {
+
     }
     if (!gl) {
-	alert("Could not initialise WebGL, sorry :-(");
+	   alert("Could not initialise WebGL, sorry :-(");
     }
 }
 
@@ -16,33 +21,33 @@ function initGL(canvas) {
 function getShader(gl, id) {
     var shaderScript = document.getElementById(id);
     if (!shaderScript) {
-	return null;
+	   return null;
     }
 
     var str = "";
     var k = shaderScript.firstChild;
     while (k) {
-	if (k.nodeType == 3) {
-	    str += k.textContent;
+	   if (k.nodeType == 3) {
+	   str += k.textContent;
 	}
-	k = k.nextSibling;
+	   k = k.nextSibling;
     }
 
     var shader;
     if (shaderScript.type == "x-shader/x-fragment") {
-	shader = gl.createShader(gl.FRAGMENT_SHADER);
+	   shader = gl.createShader(gl.FRAGMENT_SHADER);
     } else if (shaderScript.type == "x-shader/x-vertex") {
-	shader = gl.createShader(gl.VERTEX_SHADER);
+	   shader = gl.createShader(gl.VERTEX_SHADER);
     } else {
-	return null;
+	   return null;
     }
 
     gl.shaderSource(shader, str);
     gl.compileShader(shader);
 
     if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-	alert(gl.getShaderInfoLog(shader));
-	return null;
+    	alert(gl.getShaderInfoLog(shader));
+    	return null;
     }
 
     return shader;
@@ -61,7 +66,7 @@ function initShaders() {
     gl.linkProgram(shaderProgram);
 
     if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-	alert("Could not initialise shaders");
+	   alert("Could not initialise shaders");
     }
 
     gl.useProgram(shaderProgram);
@@ -94,22 +99,22 @@ function init_scene() {
 
 function draw_objects() {
     for (id in objects) {
-	draw_object(objects[id]);
+	   draw_object(objects[id]);
     }
 }
 
 function calculate_all_pos_abs() {
     for (id in objects) {
-	calculate_pos_abs(objects[id]);
+	   calculate_pos_abs(objects[id]);
     }
 }
 
 function appply_all_pos_draw(offset) {
     for (id in objects) {
-	appply_pos_draw(objects[id], offset);
+	   appply_pos_draw(objects[id], offset);
     }
     if (offset) {
-	mat4.identity(offset);
+	   mat4.identity(offset);
     }
 }
 
@@ -137,10 +142,9 @@ function webGLStart() {
     document.onkeyup = handleKeyUp;
     
     canvas.onmousedown = handleMouseDown;
-    document.onmouseup = handleMouseUp;
-    document.onmousemove = handleMouseMove;
-    
+
     tick();
+    
 }
 
 var time_last = 0;
@@ -148,8 +152,8 @@ function tick() {
     requestAnimFrame(tick);
     var time_now = new Date().getTime();
     if (time_last != 0) {
-	var time_delta = time_now - time_last;
-	handleKeys(time_delta / 1000.0);
+    	var time_delta = time_now - time_last;
+    	handleKeys(time_delta / 1000.0);
     }
     time_last = time_now;
     appply_all_pos_draw(openset_matrix)
@@ -157,6 +161,9 @@ function tick() {
 }
 
 var currentlyPressedKeys = {};
+
+var totalangle = 0;
+
 function handleKeys(time_delta) {
     //var speed = 2.4;
     // Enter
@@ -181,6 +188,7 @@ function handleKeys(time_delta) {
 	mat4.translate(openset_matrix, [-1.0 * speed * time_delta,  0.0,  0.0], openset_matrix);
     }*/
 
+
     if (currentlyPressedKeys[83]) {
         mat4.rotate(openset_matrix, (time_delta)*100 / 500, [1, 0, 0]);
     }
@@ -189,9 +197,13 @@ function handleKeys(time_delta) {
     }
     if (currentlyPressedKeys[68]) {
         mat4.rotate(openset_matrix, (time_delta)*400 / 500, [0, 1, 0]);
+        totalangle+=(time_delta)*100;
+
     }
     if (currentlyPressedKeys[65]) {
         mat4.rotate(openset_matrix, -(time_delta)*400 / 500, [0, 1, 0]);
+        totalangle-=(time_delta)*100;
+
     }
 
 
@@ -203,40 +215,24 @@ function handleKeyUp(event) {
     currentlyPressedKeys[event.keyCode] = false;
 }
 
-var mouseDown = false;
 var lastMouseX = null;
 var lastMouseY = null;
 var openset_matrix = mat4.create();
 mat4.identity(openset_matrix);
 
 function handleMouseDown(event) {
-    mouseDown = true;
     lastMouseX = event.clientX;
     lastMouseY = event.clientY;
-}
-
-
-function handleMouseUp(event) {
-    mouseDown = false;
-}
-
-var sumY = 0;
-
-function handleMouseMove(event) {
-    if (!mouseDown) {
-	   return;
+    var currangle = totalangle % 783;
+    if (Math.abs(currangle) < 20 || (783-Math.abs(currangle)) < 20) {
+        console.log(true);
+    } else {
+        console.log(false);
     }
-    var newX = event.clientX;
-    var newY = event.clientY;
+    console.log(totalangle % 783);
 
-    /*var deltaX = newX - lastMouseX;
-	mat4.rotate(openset_matrix, deltaX / 500, [0, 1, 0]);
-
-    var deltaY = newY - lastMouseY;
-    if (deltaY > 5 || deltaY < -5) {
-        mat4.rotate(openset_matrix, deltaY / 500, [1, 0, 0]);
-    }
-    lastMouseX = newX
-    lastMouseY = newY;*/
-    
+    console.log("angle" + totalangle)
+    console.log(lastMouseX);
+    console.log(lastMouseY);
 }
+
