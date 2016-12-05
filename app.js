34f0be1ae8c8e289f4de9b340c5bbd9949fbcf6b
agc123 <agc123@yeah.net>
2016-12-04 23:28:34 -0500

    handle connection lost

--- app.js
+++ app.js
@@ -19,20 +19,38 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
 
-var connection = mysql.createConnection({
+var db_config = {
 	host: 'us-cdbr-iron-east-04.cleardb.net',
 	user: 'b1741feb7c0982',
 	password: '469100d6',
 	database: 'ad_18cfb6c1d43882b'
-});
+};
 
-connection.connect(function(error) {
-	if (!!error) {
-		console.log("error");
-	} else {
-		console.log("Connected");
-	}
-})
+var connection;
+
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 
 app.use(bodyParser());
